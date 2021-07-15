01ea5b40964d77cb1bf87b46b9f7094c20f374db
19045104 <19045104@myrp.edu.sg>
2021-07-15 20:44:21 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/webservices/gettingmylocation/MainActivity.java
@@ -0,0 +1,134 @@
+package sg.edu.rp.webservices.gettingmylocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
+import androidx.fragment.app.FragmentManager;
+
+import android.Manifest;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    GoogleMap map;
+    Button btnGetLocation, btnRemoveLocation, btnRecords;
+    TextView tv;
+    FusedLocationProviderClient client;
+    LocationCallback mLocationCallback;
+    LocationRequest mLocationRequest;
+    double mlat;
+    double mlng;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnGetLocation = findViewById(R.id.btnGetLocation);
+        btnRemoveLocation = findViewById(R.id.btnRemoveLocationUpdate);
+        FragmentManager fm = getSupportFragmentManager();
+        SupportMapFragment mapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map);
+        btnRecords = findViewById(R.id.btnRecords);
+        tv = findViewById(R.id.tvLastKnownLocation);
+        client = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null) {
+                    Location data = locationResult.getLastLocation();
+                     mlat = data.getLatitude();
+                     mlng = data.getLongitude();
+                    Toast.makeText(MainActivity.this,"New Location Detected\n" + "Lat: " + mlat + ", " + " Lat: " + mlng, Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        mapFragment.getMapAsync(new OnMapReadyCallback(){
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+                LatLng Singapore = new LatLng(mlat, mlng);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(Singapore,
+                        20));
+                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+                if (permissionCheck == PermissionChecker.PERMISSION_GRANTED) {
+                    map.setMyLocationEnabled(true);
+                } else {
+                    Log.e("GMap - Permission", "GPS access has not been granted");
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+                }
+                UiSettings ui = map.getUiSettings();
+                ui.setCompassEnabled(true);
+                ui.setZoomControlsEnabled(true);
+            }
+        });
+
+        btnGetLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLocationRequest = LocationRequest.create();
+                mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                mLocationRequest.setInterval(30);
+                mLocationRequest.setFastestInterval(5000);
+                mLocationRequest.setSmallestDisplacement(500);
+                checkPermission();
+                client.requestLocationUpdates(mLocationRequest,mLocationCallback,null);
+            }
+        });
+
+        btnRemoveLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                client.removeLocationUpdates(mLocationCallback);
+            }
+        });
+
+        btnRecords.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+
+    private boolean checkPermission(){
+        int permissionCheck_Coarse = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        int permissionCheck_Fine = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionCheck_Coarse == PermissionChecker.PERMISSION_GRANTED || permissionCheck_Fine == PermissionChecker.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            return false;
+        }
+    }
+}
+
